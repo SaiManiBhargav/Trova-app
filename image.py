12797102e79e9664ad5458b66fecfb3318a2a6fe
SaiManiBhargav <42230110+SaiManiBhargav@users.noreply.github.com>
2019-10-13 23:14:13 +0530

    image.py file is been uploaded

---
+++ image.py
@@ -0,0 +1,15 @@
+from google.cloud import storage
+from firebase import firebase
+import os
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="<add your credentials path>"
+firebase = firebase.FirebaseApplication('<your firebase database path>')
+client = storage.Client()
+bucket = client.get_bucket('<your firebase storage path>')
+# posting to firebase storage
+imageBlob = bucket.blob("/")
+# imagePath = [os.path.join(self.path,f) for f in os.listdir(self.path)]
+imagePath = "<local_path>/image.png"
+imageBlob = bucket.blob("<image_name>")
+data_dir = "inception/"
+image_path = os.path.join(data_dir, 'car.jpg')
+imageBlob.download_to_filename(image_path)
